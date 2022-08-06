b5cfb6d0d40e5c7f30ff41c57ffb6040c2f9887c
mgrouch <mgrushinskiy@gmail.com>
2022-08-06 17:39:23 -0400

    speedup build

--- install-scripts/4-server/04-signalk.sh
+++ install-scripts/4-server/04-signalk.sh
@@ -64,7 +64,7 @@ export MAKEFLAGS='-j 8'
 
 ## Install signalk
 npm cache clean --force
-npm install -g pnpm@latest
+npm install -g pnpm
 
 ## Install signalk published plugins
 pushd /home/signalk/.signalk
