9df4fa4874e28a03e5a49524ff4344e56560aa98
mgrouch <mgrushinskiy@gmail.com>
2022-08-07 12:15:00 -0400

    fix

--- install-scripts/4-server/04-signalk.sh
+++ install-scripts/4-server/04-signalk.sh
@@ -62,7 +62,7 @@ install -m 644 $FILE_FOLDER/signalk.service "/etc/systemd/system/signalk.service
 ## Install signalk
 npm cache clean --force
 npm install -g pnpm@4.14.4
-pnpm install -g --unsafe-perm --production signalk-server
+npm install -g --unsafe-perm --production signalk-server
 
 ## Install signalk published plugins
 pushd /home/signalk/.signalk
