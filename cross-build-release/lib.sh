769c9c6ea6fbcf46734da81104679e296450e2b2
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 12:20:48 -0400

    coremp135 WIP

--- cross-build-release/lib.sh
+++ cross-build-release/lib.sh
@@ -103,6 +103,8 @@ inflateImage() {
     sync
     sleep 5
 
+    parted -l "$imageLocationInflated"
+
     log "resize last partition to 100%"
     partQty=$(fdisk -l "$imageLocationInflated" | grep -o "^$imageLocationInflated" | wc -l)
     parted "$imageLocationInflated" --script "resizepart $partQty 100%"
