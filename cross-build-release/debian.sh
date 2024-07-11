3337729aab02edada7116a83baa7f2bb2af5c8ed
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 16:58:16 -0400

    coremp135 WIP

--- cross-build-release/debian.sh
+++ cross-build-release/debian.sh
@@ -64,12 +64,12 @@ EOF
   # Unmount
   umountImageFile $thisArch ./work/$thisArch/"$imageName"
 
-#  ls -l ./work/$thisArch/"$imageName"
-#  wget "https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh" -P $myCache/
-#  chmod +x "$myCache"/pishrink.sh
-#  "$myCache"/pishrink.sh -s ./work/$thisArch/"$imageName" || if [ $? == 11 ]; then
-#    log "Image already shrunk to smallest size"
-#  fi
+  ls -l ./work/$thisArch/"$imageName"
+  wget "https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh" -P $myCache/
+  chmod +x "$myCache"/pishrink.sh
+  "$myCache"/pishrink.sh ./work/$thisArch/"$imageName" || if [ $? == 11 ]; then
+    log "Image already shrunk to smallest size"
+  fi
   ls -l ./work/$thisArch/"$imageName"
 
   # Renaming the OS and moving it to the release folder.
