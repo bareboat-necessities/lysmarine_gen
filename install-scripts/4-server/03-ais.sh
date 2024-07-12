c1c4668bb6e9da3a17fa4b40de4104932f071835
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 22:31:14 -0400

    coremp135 WIP

--- install-scripts/4-server/03-ais.sh
+++ install-scripts/4-server/03-ais.sh
@@ -22,7 +22,7 @@ apt-get install -y librtlsdr0 libairspy0 libairspyhf1 \
 AGENT="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 xargs -n 1 -P 2 wget --user-agent="$AGENT" -q << EOF
 https://www.free-x.de/deb4op/pool/main/a/ais-catcher-webassets/ais-catcher-webassets_20240208_all.deb
-https://www.free-x.de/deb4op/pool/main/a/ais-catcher/ais-catcher_0.5.9-deb12u1_arm64.deb
+https://www.free-x.de/deb4op/pool/main/a/ais-catcher/ais-catcher_0.5.9-deb12u1_armhf.deb
 EOF
 dpkg -i ais-catcher_*.deb ais-catcher-webassets_*.deb
 rm -rf ais-catcher*.deb
