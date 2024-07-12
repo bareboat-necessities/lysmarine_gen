e0c5e69377aca99747cd50f0466e4fd59efec148
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 22:31:40 -0400

    coremp135 WIP

--- install-scripts/4-server/03-ais.sh
+++ install-scripts/4-server/03-ais.sh
@@ -9,15 +9,9 @@
 install -v -m 0644 "$FILE_FOLDER"/rtl-ais.service "/etc/systemd/system/"
 systemctl disable rtl-ais.service
 
-
-
 # AIS-Catcher https://github.com/jvde-github/AIS-catcher
 apt-get install -y librtlsdr0 libairspy0 libairspyhf1 \
   libhackrf0 libsoapysdr0.8 libzmq3-dev libcurl4-openssl-dev zlib1g
-
-#wget -q -O - https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/AIS-catcher-20231216-bookworm-arm64.zip > AIS-catcher.zip
-#unzip AIS-catcher.zip && rm AIS-catcher.zip
-#mv AIS-catcher /usr/local/bin/ && chmod +x /usr/local/bin/AIS-catcher
 
 AGENT="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 xargs -n 1 -P 2 wget --user-agent="$AGENT" -q << EOF
