929ebd0de978e460f79d4eab40df944a515fed6f
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 22:51:15 -0400

    coremp135 WIP

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -14,9 +14,9 @@ rm -f avnav.deb
 
 wget --user-agent="$AGENT" -O avnav-history-plugin.deb https://www.free-x.de/debian/pool/main/a/avnav-history-plugin/avnav-history-plugin_20210525_all.deb
 wget --user-agent="$AGENT" -O avnav-mapproxy-plugin.deb https://www.free-x.de/debian/pool/main/a/avnav-mapproxy-plugin/avnav-mapproxy-plugin_20230214_all.deb
-wget --user-agent="$AGENT" -O avnav-sailinstrument-plugin.deb https://github.com/kdschmidt1/Sail_Instrument/releases/download/20240503/avnav-sailinstrument-plugin_20240503_all.deb
-dpkg -i avnav-history-plugin.deb avnav-mapproxy-plugin.deb avnav-sailinstrument-plugin.deb
-rm -f avnav-history-plugin.deb avnav-mapproxy-plugin.deb avnav-sailinstrument-plugin.deb
+#wget --user-agent="$AGENT" -O avnav-sailinstrument-plugin.deb https://github.com/kdschmidt1/Sail_Instrument/releases/download/20240503/avnav-sailinstrument-plugin_20240503_all.deb
+dpkg -i avnav-history-plugin.deb avnav-mapproxy-plugin.deb #avnav-sailinstrument-plugin.deb
+rm -f avnav-history-plugin.deb avnav-mapproxy-plugin.deb #avnav-sailinstrument-plugin.deb
 
 wget --user-agent="$AGENT" -O avnav-ocharts-plugin.deb https://www.free-x.de/debian/pool/main/a/avnav-ocharts-plugin/avnav-ocharts-plugin_20231216-raspbian-bookworm_arm64.deb
 wget --user-agent="$AGENT" -O avnav-ocharts.deb https://www.free-x.de/debian/pool/main/a/avnav-ocharts/avnav-ocharts_1.0.44.0-1bookworm1_arm64.deb
