a73ee456eeff9ee85d1765aca8783a8acdbfab81
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-11 21:37:52 -0400

    coremp135 WIP

--- install-scripts/4-server/04-signalk.sh
+++ install-scripts/4-server/04-signalk.sh
@@ -176,6 +176,11 @@ fi
 #sed -i "s#sudo ##g" /home/signalk/.signalk/node_modules/signalk-raspberry-pi-monitoring/index.js || true
 #sed -i "s#/opt/vc/bin/##g" /home/signalk/.signalk/node_modules/signalk-raspberry-pi-monitoring/index.js || true
 
+#TODO:
+find / -name tokensecurity.js
+find / -name modules.js
+
+
 sed -i 's#@signalk/server-admin-ui#admin#' "$(find /usr/lib/node_modules/signalk-server -name tokensecurity.js)" || true
 
 # see https://github.com/SignalK/signalk-server/pull/1455/
