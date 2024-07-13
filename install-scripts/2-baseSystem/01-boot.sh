56669e65682cb45f52cea7dbd7038e426bbae6aa
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-13 13:35:28 -0400

    coremp135 WIP cleanup

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 apt-get install -y -q parted gdisk # plymouth plymouth-label libblockdev-mdraid2
 
+if [ -f /usr/local/m5stack/init.sh ] ; then
+  install -m0644 -v "$FILE_FOLDER"/bbn_logo.jpg "/usr/local/m5stack/bbn_logo.jpg"
+  sed -i "s#m5stack/logo.jpg#m5stack/bbn_logo.jpg#g" /usr/local/m5stack/init.sh
+fi
+
 # TODO:
 exit 0
 
