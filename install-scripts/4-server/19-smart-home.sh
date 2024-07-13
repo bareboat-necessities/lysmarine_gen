8c651b2ee0a739f29082e83240c0e7532b6c7fca
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-13 17:30:53 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/19-smart-home.sh
+++ install-scripts/4-server/19-smart-home.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+#TODO
+exit 0
+
 apt-get install -y --no-install-recommends --no-install-suggests \
   python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev \
   zlib1g-dev autoconf build-essential libopenjp2-7 libtiff6 libturbojpeg0 tzdata libsqlite3-dev
