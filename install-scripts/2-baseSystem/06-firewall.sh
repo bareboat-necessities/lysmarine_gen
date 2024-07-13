eb43bde8de5bc37a9d2979e5718f97b89e5c0cd2
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-12 21:42:29 -0400

    coremp135 WIP

--- install-scripts/2-baseSystem/06-firewall.sh
+++ install-scripts/2-baseSystem/06-firewall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+#TODO
+exit 0
+
 apt-get -y -q install ufw
 
 # will be called on first boot
 install -m 755 "$FILE_FOLDER"/ufw-init.sh "/usr/local/sbin/ufw-init"
 
-echo "ip_conntrack" >> /etc/modules
