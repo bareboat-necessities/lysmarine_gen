0953e89ce380311c717464b713d71dc3969f22f5
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-13 01:49:24 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/15-pypilot.sh
+++ install-scripts/4-server/15-pypilot.sh
@@ -84,7 +84,7 @@ pushd ./stageCache
   if [[ ! -d ./pypilot ]]; then
     git clone https://github.com/pypilot/pypilot.git
     cd pypilot
-    git checkout 6b93378bd5856374b43ba8d64a96a9ccc56f84dd # Mar 13, 2024
+    git checkout f74400ad1b9613b3de8adc138e156966c95d4289 # Jul 11, 2024
     cd ..
     git clone --depth=1 https://github.com/pypilot/pypilot_data.git
     cp -rv ./pypilot_data/* ./pypilot
