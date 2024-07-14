bf1498596debc1f25d4ff78cbc3d51f64ad3bbfc
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-13 21:35:54 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/09-canboat.sh
+++ install-scripts/4-server/09-canboat.sh
@@ -6,6 +6,8 @@ yes | cpan install Config::General # for canboat n2kd_monitor
 
 systemctl disable canboat.service
 
-install -v -m 0644 "$FILE_FOLDER"/socketcan-interface.service "/etc/systemd/system/socketcan-interface.service"
+install -v -m 0644 "$FILE_FOLDER"/socketcan-interface0.service "/etc/systemd/system/socketcan-interface0.service"
+install -v -m 0644 "$FILE_FOLDER"/socketcan-interface1.service "/etc/systemd/system/socketcan-interface1.service"
 
-systemctl disable socketcan-interface.service
+systemctl disable socketcan-interface0.service
+systemctl disable socketcan-interface1.service
