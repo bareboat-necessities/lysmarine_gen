9896ee24cc8d9ad3fb36f101aa72966822e724ef
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-14 11:31:48 -0400

    coremp135 WIP cleanup

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -3,6 +3,7 @@
 apt-get install -y -q wget gnupg ca-certificates
 
 install -m 0644 -v "$FILE_FOLDER"/free-x.list "/etc/apt/sources.list.d/"
+install -m 0644 -v "$FILE_FOLDER"/debian-backports.list "/etc/apt/sources.list.d/"
 
 wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -
 
