c5caa3555cbc84c425893bd84836df0c5e3232a2
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-13 20:03:35 -0400

    coremp135 WIP cleanup

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -7,4 +7,4 @@ install -m 0644 -v "$FILE_FOLDER"/free-x.list "/etc/apt/sources.list.d/"
 wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -
 
 apt-get update  -y -q
-
+apt-get upgrade  -y -q
