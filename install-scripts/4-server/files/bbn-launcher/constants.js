92b2f1ba971e1fe037592427d7101c915a269927
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-18 18:35:25 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/files/bbn-launcher/constants.js
+++ install-scripts/4-server/files/bbn-launcher/constants.js
@@ -1,10 +1,10 @@
 module.exports = {
   commands1 : [
-    {name: 'dash', title: 'Dashboard', img: 'dashboard', bg: 'Olive', link: '/opt/kip-dash/kip-dash'},
-    {name: 'freeboard', title: 'Freeboard', img: 'chart', bg: 'SteelBlue', link: '/opt/Freeboard-sk/Freeboard-sk'},
-    {name: 'pypilot', title: 'PyPilot', img: 'autopilot', bg: 'ForestGreen', link: '/opt/Pypilot_webapp/Pypilot_webapp'},
-    {name: 'power', title: 'Power', img: 'solar', bg: 'Tan', link: 'gtk-launch'},
-    {name: 'signalk', title: 'Signal K', img: 'dashboard', bg: 'ForestGreen', link: 'gtk-launch'},
-    {name: 'autopilot', title: 'Autopilot', img: 'autopilot', bg: 'IndianRed', link: '/opt/sk-autopilot/sk-autopilot'},
+    {name: 'dash', title: 'Dashboard', img: 'dashboard', bg: 'Olive', link: ':3000/@mxtommy/kip/'},
+    {name: 'freeboard', title: 'Freeboard', img: 'chart', bg: 'SteelBlue', link: ':3000/@signalk/freeboard-sk/'},
+    {name: 'pypilot', title: 'PyPilot', img: 'autopilot', bg: 'ForestGreen', link: ':8080/'},
+    {name: 'power', title: 'Power', img: 'solar', bg: 'Tan', link: ':8000/app/'},
+    {name: 'signalk', title: 'Signal K', img: 'dashboard', bg: 'ForestGreen', link: ':3000/'},
+    {name: 'autopilot', title: 'Autopilot', img: 'autopilot', bg: 'IndianRed', link: ':3000/@signalk/signalk-autopilot/'},
   ],
 }
