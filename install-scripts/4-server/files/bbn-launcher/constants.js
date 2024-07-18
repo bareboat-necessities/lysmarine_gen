b1c398f6825a07eb0b8416ceda1093d2831f790d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-18 19:41:54 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/files/bbn-launcher/constants.js
+++ install-scripts/4-server/files/bbn-launcher/constants.js
@@ -2,9 +2,9 @@ module.exports = {
   commands1 : [
     {name: 'dash', title: 'Dashboard', img: 'dashboard', bg: 'Olive', link: ':3000/@mxtommy/kip/'},
     {name: 'freeboard', title: 'Freeboard', img: 'chart', bg: 'SteelBlue', link: ':3000/@signalk/freeboard-sk/'},
-    {name: 'pypilot', title: 'PyPilot', img: 'autopilot', bg: 'ForestGreen', link: ':8080/'},
+    {name: 'pypilot', title: 'PyPilot', img: 'race', bg: 'Peru', link: ':8080/'},
     {name: 'power', title: 'Power', img: 'solar', bg: 'Tan', link: ':8000/app/'},
-    {name: 'signalk', title: 'Signal K', img: 'dashboard', bg: 'ForestGreen', link: ':3000/'},
+    {name: 'signalk', title: 'Signal K', img: 'vessel', bg: 'ForestGreen', link: ':3000/'},
     {name: 'autopilot', title: 'Autopilot', img: 'autopilot', bg: 'IndianRed', link: ':3000/@signalk/signalk-autopilot/'},
   ],
 }
