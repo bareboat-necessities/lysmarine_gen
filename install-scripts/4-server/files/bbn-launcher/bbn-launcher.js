b2b6050350da45f8895915cfb8d91828d1b04f77
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-18 17:46:50 -0400

    coremp135 WIP cleanup

--- install-scripts/4-server/files/bbn-launcher/bbn-launcher.js
+++ install-scripts/4-server/files/bbn-launcher/bbn-launcher.js
@@ -1,13 +1,12 @@
 /*
- Copyright 2021, Bareboat Necessities
+ Copyright 2024, Bareboat Necessities
  */
-const {commands1, commands2, sites} = require('./constants');
+const {commands1} = require('./constants');
 
 const http = require('http');
 const url = require('url');
 const fileSystem = require('fs');
 const path = require('path');
-const {spawn} = require('child_process');
 
 const hostname = '127.0.0.1';
 const port = 4997;
@@ -44,11 +43,7 @@ const server = http.createServer((req, res) => {
     //console.log(`req: ${req.url}`);
     const parsed = url.parse(req.url, true);
     //console.log(`path: ${parsed.pathname}`)
-    if (parsed.pathname === '/www') {
-        writeResponse(res, 200, 'text/html', processSiteReq(parsed.query['name']));
-    } else if (parsed.pathname === '/run') {
-        writeResponse(res, 200, 'application/json', processReq(parsed), "");
-    } else if (parsed.pathname === '/img') {
+    if (parsed.pathname === '/img') {
         writeSvgResponse(res, 200, 'image/svg+xml', parsed, processMain(parsed.query['m']));
     } else if (parsed.pathname === '/js') {
         writeJsResponse(res, 200, 'text/javascript', parsed, processMain(parsed.query['m']));
@@ -75,11 +70,6 @@ function processReq(parsed) {
         let commandObj = commands1.find(value => {
             if (value.name === progName) return value
         });
-        if (!commandObj) {
-            commandObj = commands2.find(value => {
-                if (value.name === progName) return value
-            });
-        }
         if (commandObj) {
             if (commandObj.cmd != null && commandObj.cmd.length > 0) {
                 const cmd = spawn(commandObj.cmd, commandObj.args);
@@ -130,9 +120,9 @@ const style =
     '}\n' +
     '\n' +
     '.tile {\n' +
-    '    width: 122px;\n' +
-    '    margin: 3px;\n' +
-    '    padding: 3px;\n' +
+    '    width: 192px;\n' +
+    '    margin: 10px;\n' +
+    '    padding: 10px;\n' +
     '    color: White;\n' +
     '    display: inline-block;\n' +
     '    text-align: center;\n' +
@@ -145,19 +135,19 @@ const style =
     '}\n' +
     '.tile-img2 {\n' +
     '    border-radius: 14px;\n' +
-    '    width: 55px;\n' +
-    '    height: 55px;\n' +
+    '    width: 65px;\n' +
+    '    height: 65px;\n' +
     '    margin: 0 auto;\n' +
-    '    padding: 8px;\n' +
+    '    padding: 16px;\n' +
     '    text-align: center;\n' +
     '    font-family: Sans, Arial, Helvetica, sans-serif;\n' +
     '}\n' +
     '\n' +
     '.tile-img {\n' +
-    '    width: 55px;\n' +
-    '    height: 55px;\n' +
+    '    width: 65px;\n' +
+    '    height: 65px;\n' +
     '    margin: 0 auto;\n' +
-    '    padding: 8px;\n' +
+    '    padding: 16px;\n' +
     '    text-align: center;\n' +
     '    font-family: Sans, Arial, Helvetica, sans-serif;\n' +
     '}\n' +
@@ -201,31 +191,6 @@ const style =
     '}\n' +
     '\n';
 
-const script = '\n' +
-    '    <script>\n' +
-    'function run(progId) {\n' +
-    '    const http = new XMLHttpRequest();\n' +
-    '    const url=\'/run?name=\' + progId;\n' +
-    '    http.open("GET", url);\n' +
-    '    http.send();\n' +
-    '    http.onreadystatechange = () => {\n' +
-    '        console.debug(http.responseText);\n' +
-    '    }\n' +
-    '}\n' +
-    'function showPanel(prefix, id) {\n' +
-    '    const x = document.getElementById(prefix + id);\n' +
-    '    if (x.style.display === "none") {\n' +
-    '        x.style.display = "block";\n' +
-    '    } else {\n' +
-    '        x.style.display = "none";\n' +
-    '    }\n' +
-    '}\n' +
-    'function show(id) {\n' +
-    '    showPanel(\'panel\', id);\n' +
-    '    showPanel(\'next-btn\', id);\n' +
-    '}\n' +
-    '    </script>\n';
-
 function getCredits(mode) {
     let nextMode = "BW";
     if ("BW" === mode) {
@@ -240,14 +205,6 @@ function getCredits(mode) {
         '        <div style="clear:both;"></div>\n' +
         '    </div>';
 }
-
-const nextButton1 =
-    '        <div id="next-btn1" style="width: 30px; font-size: 18pt; color: white; padding: 145px 0; float: right;"\n' +
-    '            onclick="show(1); show(2);">&nbsp;&gt;&nbsp;</div>\n';
-
-const nextButton2 =
-    '        <div id="next-btn2" style="width: 30px; font-size: 18pt; color: white; padding: 145px 0; display: none; float: left;"\n' +
-    '            onclick="show(2); show(1);">&nbsp;&lt;&nbsp;</div>\n';
 
 function buildTiles(commands, mode) {
     let items = '';
@@ -280,82 +237,19 @@ function getStyle(mode) {
 }
 
 function processMain(mode) {
-    const header = '<head>\n<meta charset="UTF-8">\n' + getStyle(mode) + script +
+    const header = '<head>\n<meta charset="UTF-8">\n' + getStyle(mode) +
         '\n    <title>bbn-launcher</title>\n' +
-        '\n    <script src = "js?name=swiped-events" type = "text/javascript"></script>\n' +
-        '\n    <script>document.addEventListener(\'swipeleft\', function(e) {\n' +
-        '    show(1); show(2);\n' +
-        '});</script>\n' +
-        '\n    <script>document.addEventListener(\'swiperight\', function(e) {\n' +
-        '    show(2); show(1);\n' +
-        '});</script>\n' +
         '\n</head>\n';
     const items1 = buildTiles(commands1, mode);
     const panel1 =
         '        <div id="panel1" class="main-panel" style="float: left;">' + items1 + '\n' +
         '        </div>\n';
 
-    const items2 = buildTiles(commands2, mode);
-    const panel2 =
-        '        <div id="panel2" class="main-panel" style="display: none; float: left;">' + items2  + '\n' +
-        '        </div>\n';
-
     const panel =
-        '    <div>\n' +
-        nextButton2 + panel1 + panel2 + nextButton1 +
+        '    <div>\n' + panel1 +
         '    </div>\n' + getCredits(mode);
     const body = '<body style="background: #0C0C0C;">\n' +
         '<div style="width: 780px; height: 360px;" class="desktop">\n' + panel + '\n</div>\n</body>';
     return '<!DOCTYPE html>\n'
         + '<html lang="en">\n' + header + body + '\n</html>';
 }
-
-function processSiteReq(name) {
-    let site = sites.find(value => {
-        if (value.name === name) return value;
-    });
-    if (!site) {
-        site = sites[0];
-    }
-    return '<!DOCTYPE html>\n' +
-        '<html lang="en">\n' +
-        '<head>\n' +
-        '    <meta charset="UTF-8">\n' +
-        '    <title>' + site.name + '</title>\n' +
-        '    <style>\n' +
-        'body {\n' +
-        '    color: white;\n' +
-        '    font-family: Sans, Arial, Helvetica, sans-serif;\n' +
-        '    font-size: 24pt;\n' +
-        '}\n' +
-        '.d1 {\n' +
-        '    color: white;\n' +
-        '    margin-top: 14px;\n' +
-        '}\n' +
-        'a {\n' +
-        '    color:orange;\n' +
-        '    text-decoration:none;\n' +
-        '}\n' +
-        'a:hover, a:focus {\n' +
-        '    text-decoration:underline;\n' +
-        '}\n' +
-        '    </style>\n' +
-        '</head>\n' +
-        '<body style="background: black;">\n' +
-        '<div style="text-align: center; margin-top: 120px; ">\n' +
-        ((site.name === 'moorings') ? getMooringsLinks() : '    <div style="color: white;"><a href="' + site.url + '">Press for:&nbsp; ' + site.url + '</a></div>\n') +
-        '</div>\n' +
-        '</body>\n' +
-        '</html>\n';
-}
-
-function getMooringsLinks() {
-    return '' +
-        '    <div class="d1"><a href="https://www.noforeignland.com/">NoForeignLand.com</a></div>\n' +
-        '    <div class="d1"><a href="https://dockwa.com/">Dockwa &nbsp;(US and around)</a></div>\n' +
-        '    <div class="d1"><a href="https://www.snagaslip.com/">Snag-A-Slip &nbsp;(US and around)</a></div>\n' +
-        '    <div class="d1"><a href="https://my-sea.com/">My-Sea &nbsp;(Germany and Europe)</a></div>\n' +
-        '    <div class="d1"><a href="https://www.visitmyharbour.com/">visitMyHarbour &nbsp;(UK)</a></div>\n' +
-        '    <div class="d1"><a href="https://www.bluewaterapp.com">BlueWaterApp &nbsp;(Netherlands)</a></div>\n';
-}
-
