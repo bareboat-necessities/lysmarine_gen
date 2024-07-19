9fea1cf3cf508c8cfe9ace31b86aafddb6652369
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-19 11:28:43 -0400

    coremp135 WIP cleanup

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
-apt-get install -y -q parted gdisk
+
+apt-get install -y -q parted gdisk telnet
 
 if [ -f /etc/issue ] ; then
   install -m0664 -v "$FILE_FOLDER"/ascii_logo.txt "/etc/issue"
