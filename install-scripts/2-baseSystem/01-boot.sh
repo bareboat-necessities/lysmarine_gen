5d41b16f433b14b4823af1205d4dd41e8cbd4822
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-22 08:56:19 -0400

    fixing not rebooting without otg usb plugged in

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -11,7 +11,7 @@ if [ -f /usr/local/m5stack/init.sh ] ; then
   sed -i "s#m5stack/logo.jpg#m5stack/bbn_logo.jpg#g" /usr/local/m5stack/init.sh
 fi
 
-sed -i 's#\[ "$1" == "stop" \]#[ "z\$1" == "zstop" ]#' /etc/rc.local
+sed -i 's#\[ "$1" == "stop" \]#[ "z\$1" = "zstop" ]#' /etc/rc.local
 
 # see https://github.com/m5stack/CoreMP135_buildroot-external-st/issues/8
 systemctl disable getty@ttyGS0.service
