798129f3bb012b006282920bd5ade7eca93cb5c2
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-29 22:42:23 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -7,8 +7,6 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
 done
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
