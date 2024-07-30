f32af0332905c83d7a19fef3f43570df64b25b3d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-29 22:41:13 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -1,7 +1,27 @@
 #!/bin/bash -e
 
 # see: https://github.com/victronenergy/venus-docker
-apt-get install -y -q docker.io
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
+  do apt-get remove $pkg
+done
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -q -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 pushd /root
   git clone --recurse-submodules https://github.com/victronenergy/venus-docker
