d1482b5f2e5b4dfe658d9f28f51093c5cb56fd91
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-30 00:44:51 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -18,7 +18,7 @@ pushd /root
   pushd venus-docker
     git submodule update --init --recursive
     git submodule foreach 'git pull --ff origin master --recurse-submodules || true'
-    docker build -v /var/run/docker.sock:/var/run/docker.sock -ti "$DOCKER_CONTAINER_ID"  . -t mqtt --no-cache
+    docker build . -t mqtt --no-cache
   popd
 popd
 
