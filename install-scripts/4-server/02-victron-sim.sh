36e1e23383cb5107a945c61274de20cfbd3cb286
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-30 00:47:02 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -10,8 +10,6 @@ apt-get install -q -y ebtables docker-ce docker-ce-cli containerd.io docker-buil
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
-#docker run -v /var/run/docker.sock:/var/run/docker.sock -ti docker
-
 # see: https://github.com/victronenergy/venus-docker
 pushd /root
   git clone --recurse-submodules https://github.com/victronenergy/venus-docker
