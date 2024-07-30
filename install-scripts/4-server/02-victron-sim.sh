53668cd5dc7f15f834c674454546c577e2b98929
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-07-29 23:47:28 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -5,16 +5,16 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
   do apt-get -q -y remove $pkg
 done
 
+apt-get install -q -y ebtables docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-
-apt-get install -q -y ebtables docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # see: https://github.com/victronenergy/venus-docker
 pushd /root
   git clone --recurse-submodules https://github.com/victronenergy/venus-docker
   pushd venus-docker
-    ./build.sh || true
+    ./build.sh
   popd
 popd
 
