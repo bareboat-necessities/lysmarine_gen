c23d26299301b60d36c68b0131a61742d5a09b55
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-01 23:04:29 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -10,17 +10,17 @@ apt-get install -q -y ebtables docker-ce docker-ce-cli containerd.io docker-buil
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
-# see: https://github.com/victronenergy/venus-docker
-pushd /root
-  git clone --recurse-submodules https://github.com/victronenergy/venus-docker
-  pushd venus-docker
-    git submodule update --init --recursive
-    git submodule foreach 'git pull --ff origin master --recurse-submodules || true'
-    docker buildx create --buildkitd-flags '--allow-insecure-entitlement security.insecure' --name insecure-builder
-    docker buildx use insecure-builder
-    export DOCKER_HOST=tcp://127.0.0.1:2375
-    ls -l /var/run/docker.sock
-    docker buildx build --allow security.insecure . -t mqtt --no-cache
-  popd
-popd
+## see: https://github.com/victronenergy/venus-docker
+#pushd /root
+#  git clone --recurse-submodules https://github.com/victronenergy/venus-docker
+#  pushd venus-docker
+#    git submodule update --init --recursive
+#    git submodule foreach 'git pull --ff origin master --recurse-submodules || true'
+#    docker buildx create --buildkitd-flags '--allow-insecure-entitlement security.insecure' --name insecure-builder
+#    docker buildx use insecure-builder
+#    export DOCKER_HOST=tcp://127.0.0.1:2375
+#    ls -l /var/run/docker.sock
+#    docker buildx build --allow security.insecure . -t mqtt --no-cache
+#  popd
+#popd
 
