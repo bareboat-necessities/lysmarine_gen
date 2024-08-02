d01df3607137f7d6bc7d61f99c3aeece657ac95d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 01:11:10 -0400

    victron simulator

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -30,7 +30,7 @@ pushd venus-docker
   docker save --output victron-mqtt.tar victron-mqtt
   ls -l victron-mqtt.tar
 popd
-mv venus-docker/victron-mqtt.tar install-scripts/4-server/files/
+mv "$WORK_DIR"/venus-docker/victron-mqtt.tar install-scripts/4-server/files/
 
 
 docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker" -v "$WORK_DIR":rw -v /dev:/dev "$DOCKER_IMAGE" /bin/bash
