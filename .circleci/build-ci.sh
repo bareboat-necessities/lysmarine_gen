9e222b1853206c242d9a22b72a46e758ece4a917
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 00:39:47 -0400

    victron simulator

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -26,10 +26,11 @@ WORK_DIR=$(pwd):/ci-source
 
 git clone --recurse-submodules https://github.com/victronenergy/venus-docker
 pushd venus-docker
-  docker buildx build --output "type=docker,push=false,name=victron-mqtt,dest=./victron-mqtt.tar" . -t victron-mqtt
-  ls -l .
+  docker build . -t victron-mqtt
   docker images
   docker image ls
+  docker save --output victron-mqtt.tar victron-mqtt
+  ls -l 
 popd
 
 docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker" -v /var/run/docker.sock:/var/run/docker.sock -v "$WORK_DIR":rw -v /dev:/dev "$DOCKER_IMAGE" /bin/bash
