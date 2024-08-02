039bc80e103ee3acb6a38c75451d4977df135f5c
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 13:34:37 -0400

    victron simulator

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -27,6 +27,7 @@ WORK_DIR=$(pwd):/ci-source
 git clone --recurse-submodules https://github.com/victronenergy/venus-docker
 pushd venus-docker
   export DOCKER_DEFAULT_PLATFORM=linux/arm/v7
+  apt-get install -y npm nodejs
   docker build . -t mqtt
   docker save --output mqtt.tar mqtt
   ls -l mqtt.tar
--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -13,6 +13,11 @@ update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 install -d /root/victron
 mv "$FILE_FOLDER/mqtt.tar" "/root/victron/"
 
+o_dir=$(pwd)
+cd /root/victron
+git clone --depth=1 --recurse-submodules https://github.com/victronenergy/venus-docker
+cd "$o_dir"
+
 cat >/root/victron/readme.txt <<'EOF'
 This is Victron simulator docker image
 To load:
