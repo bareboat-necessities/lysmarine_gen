0a7dfa107bc10d319bdc645d772ffb2c24786e21
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 18:45:24 -0400

    fix gpsd

--- install-scripts/4-server/files/manage_gps.sh
+++ install-scripts/4-server/files/manage_gps.sh
@@ -3,7 +3,7 @@ chmod a+r /dev/ttyLYS_gps_"$1"
 if [[ $2 == "remove" ]] ; then
   logger "The USB device /dev/ttyLYS_gps_$1 has been disconnected"
   if [[ $1 == "0" ]] ; then
-    /usr/bin/su -c '/usr/bin/gpsdctl remove /dev/ttyLYS_gps_0'
+    /usr/bin/su -c 'gpsdctl remove /dev/ttyLYS_gps_0'
   else
     /usr/bin/su -c '/usr/bin/systemctl stop lysgpsd@'"$1"'.service'
   fi
@@ -11,7 +11,7 @@ else
   if [[ $1 == "0" ]] ; then
     /usr/bin/su -c '/usr/bin/systemctl restart gpsd'
     logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
-    /usr/bin/su -c 'GPSD_OPTIONS="-p"; export GPSD_OPTIONS; /usr/bin/gpsdctl add /dev/ttyLYS_gps_0'
+    /usr/bin/su -c 'GPSD_OPTIONS="-p"; export GPSD_OPTIONS; gpsdctl add /dev/ttyLYS_gps_0'
   else
     /usr/bin/su -c '/usr/bin/systemctl restart lysgpsd@'"$1"'.service'
     logger "This USB device is known as GPS and will be connected to gpsd on port 2947$1 /dev/ttyLYS_gps_$1"
