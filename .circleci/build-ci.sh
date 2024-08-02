151400d89c692b1072e5de8bdb73364ac7c2fe00
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 10:12:12 -0400

    victron simulator

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -26,6 +26,7 @@ WORK_DIR=$(pwd):/ci-source
 
 git clone --recurse-submodules https://github.com/victronenergy/venus-docker
 pushd venus-docker
+  export DOCKER_DEFAULT_PLATFORM=linux/armhf
   docker build . -t mqtt
   docker save --output mqtt.tar mqtt
   ls -l mqtt.tar
