ae57abedd6e32ec05d2614ae5941133bc101f2f0
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-01 23:12:01 -0400

    victron simulator

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -33,6 +33,7 @@ pushd venus-docker
   #export DOCKER_HOST=tcp://127.0.0.1:2375
   ls -l /var/run/docker.sock
   docker buildx build --allow security.insecure . -t mqtt --no-cache
+  docker images
 popd
 
 docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker" -v /var/run/docker.sock:/var/run/docker.sock -v "$WORK_DIR":rw -v /dev:/dev "$DOCKER_IMAGE" /bin/bash
