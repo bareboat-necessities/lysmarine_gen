743a22c2bdcd40764c80b8fabd605243c536a043
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 16:20:05 -0400

    victron simulator

--- cross-build-release/victron.sh
+++ cross-build-release/victron.sh
@@ -5,6 +5,8 @@ npm install cypress --save-dev
 git clone --recurse-submodules --depth=1 https://github.com/victronenergy/venus-docker
 pushd venus-docker
   export DOCKER_DEFAULT_PLATFORM=linux/arm/v7
+  sed -i '/# Html5 app/,+4d' dockerfile
+  rm -rf venus-html5-app
   docker build . -t mqtt
   docker save --output mqtt.tar mqtt
   ls -l mqtt.tar
