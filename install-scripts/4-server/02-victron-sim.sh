1de33b747e463ec2ed71fd121ae6eac7b4933baa
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 02:11:34 -0400

    victron simulator

--- install-scripts/4-server/02-victron-sim.sh
+++ install-scripts/4-server/02-victron-sim.sh
@@ -10,6 +10,6 @@ apt-get install -q -y ebtables docker-ce docker-ce-cli containerd.io docker-buil
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
-dockerd
+export DOCKER_HOST=tcp://localhost:2375
 docker load --input "$FILE_FOLDER/victron-mqtt.tar"
 docker images
