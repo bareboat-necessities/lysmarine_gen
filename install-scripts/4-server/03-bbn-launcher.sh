6d5a9f7f8db0178d7cfbffd6160645dbd60b2081
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-02 17:41:00 -0400

    victron simulator

--- install-scripts/4-server/03-bbn-launcher.sh
+++ install-scripts/4-server/03-bbn-launcher.sh
@@ -23,4 +23,4 @@ chown -R www-data:www-data /var/www/bbn-launcher
 
 install -v -m 0644 "$FILE_FOLDER"/bbn-launcher.service "/etc/systemd/system/"
 
-systemctl enable bbn-launcher
+systemctl disable bbn-launcher
