312a4967cc175142570a9da666c657c116e56ef6
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-10-08 23:44:07 -0400

    upd

--- install-scripts/8-apps/01-boatinstrument.sh
+++ install-scripts/8-apps/01-boatinstrument.sh
@@ -3,6 +3,8 @@
 install -o 1000 -g 1000 -v "$FILE_FOLDER"/boatinstrument.json "/root/boatinstrument.json"
 install -o 1000 -g 1000 -v "$FILE_FOLDER"/boatinstrument.json "/root/boatinstrument.json-bbn"
 
+apt-get install -y libinput10 libvulkan1 libgstreamer-plugins-base1.0-0
+
 BK_DIR="$(pwd)"
 
 cd /root
@@ -15,3 +17,5 @@ cd "$BK_DIR"
 
 install -d /etc/systemd/system
 install -m 644 "$FILE_FOLDER"/boatinstrument-flutter-pi.service "/etc/systemd/system/boatinstrument-flutter-pi.service"
+
+systemctl disable boatinstrument-flutter-pi
