77b7ec8850ceed5f7b76f18d49cdbada97cabf0b
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-10-08 22:52:02 -0400

    upd

---
+++ install-scripts/8-apps/01-boatinstrument.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+install -o 1000 -g 1000 -v "$FILE_FOLDER"/boatinstrument.json "/root/boatinstrument.json"
+install -o 1000 -g 1000 -v "$FILE_FOLDER"/boatinstrument.json "/root/boatinstrument.json-bbn"
+
+BK_DIR="$(pwd)"
+
+cd /root
+
+wget -O boatinstrument.tgz https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/boatinstrument-0.1.0.1-flutterpi_arm32.tgz
+gzip -cd < boatinstrument.tgz | tar xvf -
+rm -f boatinstrument.tgz
+
+cd "$BK_DIR"
+
+install -d /etc/systemd/system
+install -m 644 "$FILE_FOLDER"/boatinstrument-flutter-pi.service "/etc/systemd/system/boatinstrument-flutter-pi.service"
