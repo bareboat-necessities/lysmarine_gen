d6554d6eafd26b148f7072bb36d259797e11dbe5
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-03-30 15:43:33 -0400

    upd

--- cross-build-release/lib.sh
+++ cross-build-release/lib.sh
@@ -97,8 +97,8 @@ inflateImage() {
       log "truncate image to 9.2G"
       truncate -s "9216M" "$imageLocationInflated"
     else
-      log "truncate image to 15.5G"
-      truncate -s "15488M" "$imageLocationInflated"
+      log "truncate image to 16G"
+      truncate -s "16G" "$imageLocationInflated"
     fi
 
     log "resize last partition to 100%"
