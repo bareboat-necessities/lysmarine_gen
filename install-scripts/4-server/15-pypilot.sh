aeb5ba2bb16ab75c08e94be2524b8035fa3db81e
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-04-07 14:07:47 -0400

    mast height correction fix

--- install-scripts/4-server/15-pypilot.sh
+++ install-scripts/4-server/15-pypilot.sh
@@ -89,7 +89,7 @@ pushd ./stageCache
   if [[ ! -d ./pypilot ]]; then
     git clone https://github.com/pypilot/pypilot.git
     cd pypilot
-    git checkout 6b93378bd5856374b43ba8d64a96a9ccc56f84dd # Mar 13, 2024
+    git checkout f66ae9a518a12a70b762cff03bf36e971d498b79 # Apr 2, 2025
     cd ..
     git clone --depth=1 https://github.com/pypilot/pypilot_data.git
     cp -rv ./pypilot_data/* ./pypilot
