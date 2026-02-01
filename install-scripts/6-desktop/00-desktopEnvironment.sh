c90e3bbc3fe575d9462a2655fbee48a8d0fabeb6
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-02-01 18:07:18 -0500

    trixie

--- install-scripts/6-desktop/00-desktopEnvironment.sh
+++ install-scripts/6-desktop/00-desktopEnvironment.sh
@@ -5,17 +5,22 @@ install  -v "$FILE_FOLDER"/Xwrapper.config "/etc/X11/"
 
 arch=$(dpkg --print-architecture)
 
+
+apt-get install -y -q \
+
+
 # Install touchscreen drivers, etc
 apt-get -q -y install xserver-xorg-input-libinput xinput libinput-tools xinput-calibrator gldriver-test \
  budgie-desktop budgie-weathershow-applet budgie-rotation-lock-applet \
- gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base \
+ gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+ gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-v4l2 \
  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa \
  gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git \
  xsettingsd xserver-xorg \
  xinit cpanminus perl-base wmctrl openbox python3-xdg arandr gnome-clocks \
  dialog lxterminal network-manager-gnome system-config-printer \
  lxterminal gpsbabel file-roller lxtask thunar git \
- libqt5quickwidgets5 libqt5widgets5 libqt5gui5 libqt5webkit5 \
+ libqt5quickwidgets5 libqt5widgets5 libqt5gui5 libqt5webenginewidgets5 libqt5webengine-data \
  libqt5sql5 libqt5printsupport5 libqt5network5 libqt5serialport5 \
  libqt5svg5 libqt5opengl5 libqt5test5 libqt5xml5 libqt5qml5 qml-module-qtquick-controls libsndfile1
 
