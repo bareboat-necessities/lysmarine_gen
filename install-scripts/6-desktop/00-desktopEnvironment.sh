00a53786cb8d15a46b50b9626ffec3e7f59fa196
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-02-02 21:03:13 -0500

    opencpn

--- install-scripts/6-desktop/00-desktopEnvironment.sh
+++ install-scripts/6-desktop/00-desktopEnvironment.sh
@@ -1,39 +1,23 @@
 #!/bin/bash -e
 
-## Needed to allow the service file start X
-install  -v "$FILE_FOLDER"/Xwrapper.config "/etc/X11/"
+install  -v "$FILE_FOLDER"/Xwrapper.config "/etc/X11/"  # Needed to allow the service file start X
 
-arch=$(dpkg --print-architecture)
-
-
-apt-get install -y -q \
-
-
-# Install touchscreen drivers, etc
 apt-get -q -y install xserver-xorg-input-libinput xinput libinput-tools xinput-calibrator gldriver-test \
  budgie-desktop budgie-weathershow-applet budgie-rotation-lock-applet \
  gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav \
  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa v4l-utils \
  gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git \
- xsettingsd xserver-xorg \
+ xsettingsd xserver-xorg xserver-xorg-video-fbturbo \
  xinit cpanminus perl-base wmctrl openbox python3-xdg arandr gnome-clocks \
  dialog lxterminal network-manager-gnome system-config-printer \
  lxterminal gpsbabel file-roller lxtask thunar git \
  libqt5quickwidgets5 libqt5widgets5 libqt5gui5 libqt5webenginewidgets5 libqt5webengine-data \
  libqt5sql5 libqt5printsupport5 libqt5network5 libqt5serialport5 \
  libqt5svg5 libqt5opengl5 libqt5test5 libqt5xml5 libqt5qml5 qml-module-qtquick-controls libsndfile1 \
+ chromium chromium-common chromium-sandbox rpi-chromium-mods \
+ lxterminal gpsbabel file-roller lxtask thunar \
  libgtkmm-3.0-1t64 libglibmm-2.4-1t64 libatkmm-1.6-1v5 libpangomm-1.4-1v5 libcairomm-1.0-1v5
-
-
-#if [ "$LMOS" == Raspbian ]; then
-  # For raspberry
-  apt-get -q -y install xserver-xorg-video-fbturbo
-#fi
-
-if [ "$LMOS" == Armbian ]; then
-	apt-get -q -y install xserver-xorg-legacy
-fi
 
 install -o 1000 -g 1000 -d /home/user/.local
 install -o 1000 -g 1000 -d /home/user/.local/share
@@ -73,19 +57,7 @@ install -v "$FILE_FOLDER"/commands.desktop /usr/local/share/applications/
 install -d /etc/budgie-desktop
 install -m 644 -v "$FILE_FOLDER"/panel.ini /etc/budgie-desktop/
 
-
-# Make some room for the rest of the build script
-apt-get clean
-
-## Install www apps.
-apt-get install -y -q chromium chromium-common chromium-sandbox rpi-chromium-mods
-
-# Adobe Flash Player. Copyright 1996-2015. Adobe Systems Incorporated. All Rights Reserved.
-#DEBIAN_FRONTEND=noniterractive apt-get -o Dpkg::Options:=="--force-confnew" -q -y install rpi-chromium-mods
-
-apt-get install -y -q lxterminal gpsbabel file-roller lxtask thunar
-
-#apt-get install -y -q pcmanfm mousepad
+apt-get clean   # Make some room for the rest of the build script
 
 install -v "$FILE_FOLDER"/scale-up.desktop /usr/local/share/applications/
 install -v "$FILE_FOLDER"/scale-down.desktop /usr/local/share/applications/
@@ -94,9 +66,6 @@ install -v -m 755 "$FILE_FOLDER"/scale-up /usr/local/bin/
 install -v -m 755 "$FILE_FOLDER"/scale-down /usr/local/bin/
 
 install -v -m 755 "$FILE_FOLDER"/twofing-detect.sh /usr/local/sbin/twofing-detect
-
-# force polkit agent to start with openbox (this is needed for nm-applet hotspot)
-#sed -i '/^OnlyShowIn=/ s/$/GNOME;Budgie;/' /etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop
 
 # replace budgie-wm with openbox
 sed -i 's/org.buddiesofbudgie.BudgieWm;//' /usr/share/gnome-session/sessions/org.buddiesofbudgie.BudgieDesktop.session
